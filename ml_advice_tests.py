ece5fd4772f8ce64bc2f3a2063d720af146225fa
DanielCruzDataEngineer <danielcruz.alu.lmb@gmail.com>
2023-02-09 11:39:16 -0300

    Data Changes

--- ml_advice_tests.py
+++ ml_advice_tests.py
@@ -66,13 +66,13 @@ def checkPrice(itens):
 mon_1 = 'https://produto.mercadolivre.com.br/MLB-3087768923-monitor-led-22-hq-widescreen-vesa-ajuste-de-inclinaco-_JM#position=33&search_layout=grid&type=item&tracking_id=788e07de-37da-428e-a72d-ac4f0a096115'
 mon_2 = 'https://produto.mercadolivre.com.br/MLB-1957277796-monitor-com-bordas-ultrafinas-215-221v8-hdmi-philips-_JM#position=31&search_layout=grid&type=item&tracking_id=199fcff5-ed27-484b-90ec-04879210a753'
 mon_3 = 'https://www.mercadolivre.com.br/monitor-aoc-b1-series-24b1xhm-led-238-preto-90v240v/p/MLB18459540?pdp_filters=seller_id%3A480263032#reco_item_pos=0&reco_backend=machinalis-seller-items-pdp&reco_backend_type=low_level&reco_client=vip-seller_items-above&reco_id=d8fca78d-590a-4345-ad83-4a9e5dc97c95'
-mon_4 = 'https://www.mercadolivre.com.br/monitor-gamer-lg-24mp400-lcd-238-preto-100v240v/p/MLB18910464#reco_item_pos=2&reco_backend=univb-pdp&reco_backend_type=low_level&reco_client=pdp-v2p&reco_id=624cddcf-a1a8-4b66-83ef-511f44fd3a1c'
+mon_4 = 'https://produto.mercadolivre.com.br/MLB-3175688494-monitor-led-23-full-hd-hdmi-vga-bivolt-110v-220v-preto-_JM#is_advertising=true&position=23&search_layout=grid&type=pad&tracking_id=7a5f0fea-c0b1-4936-8f23-336b2897db91&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=23&ad_click_id=YmUxYjhiOWYtNmQ1ZS00NjI2LTg4ZjQtODY0OTI1OWE1MDMx'
 
 # Não se esqueça de também adicionar o item aqui, junto com o preço
 itens = [{'url': mon_1, 'price': 599.0, 'email': False, 'store': 'mercado'},
-{'url': mon_2, 'price': 699.0, 'email': False, 'store': 'mercado'}, #689
+{'url': mon_2, 'price': 689.0, 'email': False, 'store': 'mercado'}, #689
 {'url': mon_3, 'price': 759.0, 'email': False, 'store': 'mercado'},
-{'url': mon_4, 'price': 829.0, 'email': False, 'store': 'mercado'}
+{'url': mon_4, 'price': 845.0, 'email': False, 'store': 'mercado'}
 ]
 
 
